521a149215cf6b2e2ae585ba29eb98384ba38091
LeviLeal <levyfleal@gmail.com>
2025-01-15 15:21:57 -0300

    fastapi adicionad

--- main.py
+++ main.py
@@ -1,13 +1,7 @@
 from fastapi import FastAPI
-from contextlib import asynccontextmanager
 from sqlmodel import SQLModel, Field, create_engine, Session
 
-
 app = FastAPI()
-
-@asynccontextmanager
-async def lifespan(app: FastAPI):
-    create_db_and_tables()
 
 # home page
 @app.get("/")
